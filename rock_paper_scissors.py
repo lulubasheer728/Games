6e13c90819189303f9a9ce2f135565b446620209
Lulu <lulubasheer.pm@gmail.com>
2024-12-15 20:51:00 +0100

    Added rock,paper,scissors game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,57 @@
+import random
+from enum import IntEnum
+
+
+class Action(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+
+
+def get_user_selection():
+    choices = [f"{action.name}[{action.value}]" for action in Action]
+    choices_str = ", ".join(choices)
+    selection = int(input(f"Enter a choice ({choices_str}): "))
+    action = Action(selection)
+
+    return action
+
+
+def get_computer_selection():
+    selection = random.randint(0, len(Action) - 1)
+    action = Action(selection)
+
+    return action
+
+
+def determine_winner(user_action: int, computer_action: int):
+    victories = {
+        Action.Rock: [Action.Scissors],  # Rock beats scissors
+        Action.Paper: [Action.Rock],  # Paper beats rock
+        Action.Scissors: [Action.Paper]  # Scissors beats paper
+    }
+
+    defeats = victories[user_action]
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif computer_action in defeats:
+        print(f"{user_action.name} beats {computer_action.name}! You win!")
+    else:
+        print(f"{computer_action.name} beats {user_action.name}! You lose.")
+
+
+while True:
+    try:
+        user_action = get_user_selection()
+    except ValueError as e:
+        range_str = f"[0, {len(Action) - 1}]"
+        print(f"Invalid selection. Enter a value in range {range_str}")
+        continue
+
+    computer_action = get_computer_selection()
+    determine_winner(user_action, computer_action)
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
